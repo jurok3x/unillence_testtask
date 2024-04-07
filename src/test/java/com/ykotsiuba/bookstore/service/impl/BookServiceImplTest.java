c2453bf2f476af9fc186e8765353496b666a54a6
ykotsiuba <jurok3x@gmail.com>
2024-04-07 13:52:12 +0300

    Updated unit tests.

--- src/test/java/com/ykotsiuba/bookstore/service/impl/BookServiceImplTest.java
+++ src/test/java/com/ykotsiuba/bookstore/service/impl/BookServiceImplTest.java
@@ -1,8 +1,8 @@
 package com.ykotsiuba.bookstore.service.impl;
 
-import com.ykotsiuba.bookstore.BookOuterClass;
 import com.ykotsiuba.bookstore.dto.BookDTO;
 import com.ykotsiuba.bookstore.dto.CreateBookRequestDTO;
+import com.ykotsiuba.bookstore.dto.UpdateBookRequestDTO;
 import com.ykotsiuba.bookstore.entity.Book;
 import com.ykotsiuba.bookstore.mapper.BookMapper;
 import com.ykotsiuba.bookstore.mapper.BookMapperImpl;
@@ -12,9 +12,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -52,7 +55,7 @@ class BookServiceImplTest {
                 .save(any(Book.class)))
                 .willReturn(book);
 
-        CreateBookRequestDTO request = prepareRequest();
+        CreateBookRequestDTO request = prepareCreateRequest();
         Mono<BookDTO> savedBookMono = service.save(request);
 
         assertNotNull(savedBookMono);
@@ -63,6 +66,33 @@ class BookServiceImplTest {
                 .verify();
 
         verify(repository).save(any(Book.class));
+    }
+
+    @Test
+    void whenUpdateBook_thenReturnCorrectBook() {
+        Book book = prepareBook();
+        Book updatedBook = prepareBook();
+        UpdateBookRequestDTO request = prepareUpdateRequest();
+        updatedBook.setAuthor(request.getAuthor().get());
+        given(repository
+                .findById(any(UUID.class)))
+                .willReturn(Optional.of(book));
+        given(repository
+                .save(any(Book.class)))
+                .willReturn(updatedBook);
+
+
+        Mono<BookDTO> updatedBookMono = service.update(book.getId().toString(), request);
+
+        assertNotNull(updatedBookMono);
+
+        StepVerifier.create(updatedBookMono)
+                .expectNextMatches(savedBook -> savedBook.getAuthor().equals(request.getAuthor().get()))
+                .expectComplete()
+                .verify();
+
+        verify(repository).save(any(Book.class));
+        verify(repository).findById(any(UUID.class));
     }
 
     @Test
@@ -84,12 +114,63 @@ class BookServiceImplTest {
         verify(repository).findById(any(UUID.class));
     }
 
-    private CreateBookRequestDTO prepareRequest() {
+    @Test
+    void whenFindAllBooks_thenReturnCorrectBookList() {
+        Book book = prepareBook();
+        List<Book> books = Arrays.asList(book, book, book);
+        given(repository
+                .findAll())
+                .willReturn(books);
+
+        Flux<BookDTO> bookDTOFlux = service.findAll();
+
+        assertNotNull(bookDTOFlux);
+
+        StepVerifier.create(bookDTOFlux)
+                .expectNextCount(books.size())
+                .expectComplete()
+                .verify();
+
+        verify(repository).findAll();
+    }
+
+    @Test
+    void whenDeleteBook_thenExecuteCorrectly() {
+        Book book = prepareBook();
+        given(repository
+                .findById(any(UUID.class)))
+                .willReturn(Optional.of(book));
+        doNothing()
+                .when(repository)
+                .deleteById(any(UUID.class));
+
+        Mono<Void> deleteResultMono = service.delete(book.getId().toString());
+
+        assertNotNull(deleteResultMono);
+
+        StepVerifier.create(deleteResultMono)
+                .expectComplete()
+                .verify();
+
+        verify(repository).deleteById(any(UUID.class));
+        verify(repository).findById(any(UUID.class));
+    }
+
+    private CreateBookRequestDTO prepareCreateRequest() {
         return CreateBookRequestDTO.builder()
                 .author("John Doe")
                 .title("Title")
                 .isbn("ISBN10000100")
                 .quantity(10)
+                .build();
+    }
+
+    private UpdateBookRequestDTO prepareUpdateRequest() {
+        return UpdateBookRequestDTO.builder()
+                .author(Optional.of("New Author"))
+                .title(Optional.empty())
+                .isbn(Optional.empty())
+                .quantity(Optional.empty())
                 .build();
     }
 
