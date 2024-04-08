2046763d4c2a01235a5ca67b7bbf5c0c2f3302b9
ykotsiuba <jurok3x@gmail.com>
2024-04-08 16:11:32 +0300

    Fixed tests.

--- src/test/java/com/ykotsiuba/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/ykotsiuba/bookstore/BookstoreApplicationTests.java
@@ -2,8 +2,15 @@ package com.ykotsiuba.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@Import(TestBookstoreApplication.class)
+@TestPropertySource(properties = {
+		"spring.liquibase.enabled=true",
+		"spring.liquibase.change-log=classpath:db/changelog/test-changelog-master.xml"
+})
 class BookstoreApplicationTests {
 
 	@Test
